55b1cc6ddac8c30f9e09d6bbe6d1fcb56c70f295
hyungjun-tech01 <whmoon00@naver.com>
2024-02-19 21:42:45 +0900

    table 작성중

---
+++ db_tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE company                      
+  (id serial PRIMARY KEY,                   
+  company_code varchar(32),                 
+  customer_code varchar(32),                
+  business_code varchar(32),                
+  company_number varchar(50),               
+  group_ varchar(255),                      
+  relationship varchar(50),                
+  registration_date date,                   
+  head_quater_responsible varchar(50),      
+  Business_strategy_area varchar(50),     
+  Corporate_number varchar(50),            
+  group_classification varchar(100),       
+  Transaction_classification varchar(100), 
+  company_name varchar(50),                 
+  company_name1 varchar(50),                
+  english_company_name varchar(100),        
+  Business_number  varchar(50)        
+);
+
+
