a8ab29cb1e61f90dcb82281c4d3a9536bcbaea55
= <=>
2024-03-27 09:06:33 +0900

    [Consulting] 모든 값 가져오기 추가

--- server.js
+++ server.js
@@ -225,6 +225,24 @@ app.get('/leads', async(req, res) => {
     }
 });
 
+app.get('/consultings', async(req, res) => {
+    try{
+        console.log("[Get] consultings");
+        const allConsultingsResult = await pool.query(`
+            select * from tbl_consulting_info`);
+
+        if(allConsultingsResult.rows.length > 0) {
+            const allConsultings = allConsultingsResult.rows;
+            res.json(allConsultings);
+            res.end();
+        };
+    }catch(err){
+        console.log(err);
+        res.json({message:err});        
+        res.end();
+    }
+});
+
 
 //create/update company 
 app.post('/modifyCompany', async(req, res) => {
