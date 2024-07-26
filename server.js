a50c6b1cc393f8480134385469e2781cafcbc3d6
hyungjunac <whmoon00@naver.com>
2024-07-26 17:22:15 +0900

    contract update 버그 수정

--- server.js
+++ server.js
@@ -681,6 +681,7 @@ app.post('/purchaseMaContract', async(req, res) => {
 
         if(purchaseMaContractResult.rows.length > 0) {
             const purchaseMaContract = purchaseMaContractResult.rows;
+            console.log(purchaseMaContract);
             res.json(purchaseMaContract);
             res.end();
         }else{
@@ -2100,7 +2101,7 @@ app.post('/modifyQuotation', async(req, res) => {
 app.post('/modifyMaContract', async(req, res) => {
     const  { 
         action_type                = defaultNull(req.body.action_type),
-        guid                       = defaultNull(req.body.guid),
+        ma_code                       = defaultNull(req.body.ma_code),
         purchase_code              = defaultNull(req.body.purchase_code),
         ma_company_code            = defaultNull(req.body.ma_company_code),
         ma_contract_date           = defaultNull(req.body.ma_contract_date),
@@ -2114,7 +2115,7 @@ app.post('/modifyMaContract', async(req, res) => {
 
         const current_date = await pool.query(`select to_char(now(),'YYYY.MM.DD HH24:MI:SS') currdate`);
         const currentDate = current_date.rows[0];
-        let v_guid = guid;
+        let v_ma_code = ma_code;
 
         if (modify_user === null ){
             throw new Error('modify user는 not null입니다.');
@@ -2133,9 +2134,9 @@ app.post('/modifyMaContract', async(req, res) => {
             if (ma_company_code === null || ma_company_code === "") {
                 throw new Error('ma_company_code는 not null입니다.');
             }
-            v_guid  = pk_code();
+            v_ma_code  = pk_code();
             const response = await pool.query(`insert into tbl_MA_contract(
-                guid                      ,
+                ma_code                      ,
                 purchase_code             ,
                 ma_company_code           ,
                 ma_contract_date          ,
@@ -2148,7 +2149,7 @@ app.post('/modifyMaContract', async(req, res) => {
                 ma_modify_date            )
                 values($1,$2,$3,$4::date,$5::date,$6::numeric,$7,$8,$9::timestamp,$10,$11::timestamp)`,
                 [
-                    v_guid                ,
+                    v_ma_code                ,
                     purchase_code         ,
                     ma_company_code       ,
                     ma_contract_date      ,
@@ -2174,7 +2175,7 @@ app.post('/modifyMaContract', async(req, res) => {
                     ma_memo                  = COALESCE($6, ma_memo) ,
                     ma_recent_user           = COALESCE($7, ma_recent_user) ,
                     ma_modify_date           = COALESCE($8::timestamp, ma_modify_date) 
-                where guid = $9
+                where ma_code = $9
             `,[
                 purchase_code                ,
                 ma_company_code              ,
@@ -2184,20 +2185,20 @@ app.post('/modifyMaContract', async(req, res) => {
                 ma_memo                      ,
                 modify_user                  ,
                 currentDate.currdate         ,
-                v_guid
+                v_ma_code
             ]);            
         }
 
-        const out_guid = v_guid;
+        const out_ma_code = v_ma_code;
         const out_create_user = action_type === 'ADD' ? modify_user : "";
         const out_create_date = action_type === 'ADD' ? currentDate.currdate : "";
         const out_modify_date = currentDate.currdate;
         const out_recent_user = modify_user;
         
-        res.json({ out_guid: out_guid,  out_create_user:out_create_user, 
+        res.json({ out_ma_code: out_ma_code,  out_create_user:out_create_user, 
            out_create_date:out_create_date, out_modify_date:out_modify_date, out_recent_user:out_recent_user }); // 결과 리턴을 해 줌 .  
    
-        console.log({ out_guid: out_guid,  out_create_user:out_create_user, 
+        console.log({ action_type:action_type, out_ma_code: out_ma_code,  out_create_user:out_create_user, 
                out_create_date:out_create_date, out_modify_date:out_modify_date, out_recent_user:out_recent_user });
 
     }catch(err){
