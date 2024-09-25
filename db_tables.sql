12032a8dd02c00293ac84f342a91fd77e95098ff
C.H.S <newtons2002@gmail.com>
2024-09-25 22:32:31 +0900

    consulting table에 첨부파일 관련 column 추가

--- db_tables.sql
+++ db_tables.sql
@@ -2520,3 +2520,7 @@ CREATE SEQUENCE quotation_reset_seq START 1;
 --2024.09.24
 nvm install 20.5.0
 nvm use 20.5.0
+
+--2024.09.25
+alter table tbl_consulting_info add column request_attachment_code varchar(36);
+alter table tbl_consulting_info add column action_attachment_code varchar(36);
