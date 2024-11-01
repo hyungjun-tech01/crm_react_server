74f43c3331ba939442255f798ac8510ea8195b79
C.H.S <newtons2002@gmail.com>
2024-11-01 22:07:47 +0900

    인감 파일 부분 삭제

--- server.js
+++ server.js
@@ -73,7 +73,6 @@ app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded( {extended : false } ));
 app.use('/uploads', express.static('uploads'));
-app.use('/files', express.static('files'));
 
 // req body 전송 한계 용량을 늘려
 //app.use(express.json({ limit: '25mb' })); // JSON 데이터에 대해 50MB로 제한
