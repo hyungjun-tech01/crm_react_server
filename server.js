7d9a2a90782db068684f7c32ab69c3f1e7bc603a
hyungjunac <whmoon00@naver.com>
2024-11-03 22:01:27 +0900

    delete company 작업 중

--- server.js
+++ server.js
@@ -1355,7 +1355,15 @@ app.post('/modifyCompany', async(req, res) => {
                 sales_resource,application_engineer,region,site_id, v_company_code
             ]);
            // update 
-        }  
+        }
+        if (action_type === 'DELETE') {
+            const response = await pool.query(`
+                delete from tbl_company_info 
+                where company_code = $1
+                and modify_user = $2;
+            `,[v_company_code, modify_user
+            ]);  // delete 
+        }
         const out_company_code = v_company_code;
         const out_create_user = action_type === 'ADD' ? modify_user : "";
         const out_create_date = action_type === 'ADD' ? currentDate.currdate:"";
